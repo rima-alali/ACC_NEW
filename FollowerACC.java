94cd6657fc9bc8b173d0405dedb169cf86c14048
rima-alali <alali@d3s.mff.cuni.cz>
2013-10-03 11:03:54 +0200

    time 2

--- FollowerACC.java
+++ FollowerACC.java
@@ -77,7 +77,7 @@ public class FollowerACC extends Component {
 			fLSpeedMin.value = fLSpeed;
 			fLSpeedMax.value = fLSpeed;
 
-			fLTimePeriod = timePeriodInSeconds + (currentTime - fLCreationTime);
+			fLTimePeriod = (currentTime - fLCreationTime);
 			System.out.println("f time period :"+fLTimePeriod);
 			for (int i = 0; i < fLTimePeriod; i++) {
 					boundaries = vehilceKnowledgeEvolutionModel(fLSpeedMin.value, fLSpeedMax.value, fLPosMin.value, fLPosMax.value, ACCDatabase.lTorques);
